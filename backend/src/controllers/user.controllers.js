6d230fb6baad360a970379b54ac5b58b0c684580
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-03-25 18:40:22 +0530

    feat changes -added functions for generating refresh and access tokens from the user's model -added new controllers for user model

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -6,20 +6,122 @@ import { UserSchema } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { generateAccessAndRefreshTokens } from "../utils/TokenGenerator.js";
+import Jwt from "jsonwebtoken";
 
 const createUser = asyncHandler(async (req, res) => {
-  const contactNumber = req.body;
+  const { contactNumber } = req.body;
 
-  const user = await UserSchema.find({ contactNumber });
-  //   if(user) call the login function here it self
-  if (!user)
+  const user = await UserSchema.findOne({ contactNumber });
+  if (user)
     throw new ApiError(
       400,
-      "New user please enter the OTP we shared on your contact number ",
+      "User already registered with this contact number, please login",
     );
 
+  const newUser = await UserSchema.create({
+    contactNumber,
+  });
+
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  // this step will ask for having a sms function.
+  newUser.otp = otp;
+  await newUser.save();
+
+  res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        otp,
+        newUser,
+      },
+      "User registered successfully",
+    ),
+  );
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const contactNumber  = req.body;
+
+  const user = await UserSchema.findOne(contactNumber);
+  if (!user) throw new ApiError(404, "User not found");
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  user.otp = otp;
+  await user.save();
+
+  res.status(201).json(
+    new ApiResponse(
+      201,
+      {
+        otp,
+        user,
+      },
+      "User registered successfully",
+    ),
+  );
+});
+
+const verifyUserOtp = asyncHandler(async (req, res) => {
+  const { contactNumber, otp, userId } = req.body;
+
+  if (!contactNumber || !otp || !userId) {
+    throw new ApiError(404, "Invalid request please try again later !");
+  }
+
+  const user = await UserSchema.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found please try again");
+  }
+
+  if (String(user.otp) !== String(otp)) {
+    // Delete wrong registration
+    await UserSchema.findOneAndDelete(contactNumber);
+
+    throw new ApiError(
+      401,
+      "Invalid OTP. Registration cancelled. Please register again.",
+    );
+  }
+
+  user.otp = undefined;
+
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user, tokens: { accessToken, refreshToken } },
+        "OTP verified successfully !",
+      ),
+    );
+});
+
+const refreshUserToken = asyncHandler(async (req, res) => {
+  const token = req.body.refreshToken;
+
+  if (!token) throw new ApiError(401, "Unauthorized request");
+
+  const decoded = Jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+
+  const user = await UserSchema.findById(decoded._id);
+  console.log("before condition", decoded._id);
+  if (!user) throw new ApiError(401, "Invalid refresh token");
+  console.log("after condition", decoded._id);
+
+  const { AccessToken, RefreshToken } = await generateAccessAndRefreshTokens(
+    user._id,
+    "User",
+  );
+
+  return res.status(201).json(
+    new ApiResponse(201, {
+      user: user,
+      tokens: { AccessToken, RefreshToken },
+    }),
+  );
+});
+
+export { createUser, verifyUserOtp, refreshUserToken, loginUser };
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
     email: { type: String, trim: true },
-    contactNumber: { type: String, trim: true },
+    contactNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^[6-9]\d{9}$/.test(v);
+        },
+        message: "Invalid contact number",
+      },
+    },
     address: { type: String },
     city: {
       type: mongoose.Schema.Types.ObjectId,
@@ -21,5 +33,21 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, role: "User" },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id, role: "User" },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" },
+  );
+};
 
 export const UserSchema = mongoose.model("UserSchema", userSchema);
