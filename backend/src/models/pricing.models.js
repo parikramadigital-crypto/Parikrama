9c4fc7d2059c14cac2311ea99538a8c1ee6ec66b
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-29 16:14:31 +0530

    feat: add Pricing model with schema for pricing details and features

---
+++ backend/src/models/pricing.models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const pricingSchema = new mongoose.Schema(
+  {
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    modelFor: {
+      type: String,
+      enum: ["User", "Facilitator", "Food Place", "Hotel", "Custom"],
+      default: "Food Place",
+      required: true,
+    },
+    customModelFor: String,
+    customModel: { type: Boolean, default: false },
+    modelName: {
+      type: String,
+      enum: ["Free", "Starter", "Standard", "Professional", "Enterprise"],
+      default: "Free",
+    },
+    tagline: String,
+    price: {
+      mrp: { type: Number, required: true },
+      discount: Number,
+      sellingPrice: Number,
+    },
+    planDuration: {
+      type: String,
+      enum: ["Quarter", "Half-Year", "Per-Year"],
+      default: "Per-Year",
+    },
+    features: [String],
+    faqs: [{ question: String, answer: String }],
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+export const Pricing = mongoose.model("Pricing", pricingSchema);
